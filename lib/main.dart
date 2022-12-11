b643ab111107892b67a2735ba947c00385301097
Dfg_Dfgh <dd180841@gmail.com>
2022-12-11 15:29:04 +0000

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:lafyuu/Screens/homeScreen.dart';
+import 'package:lafyuu/Screens/main_.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Homepage(),
+      home: MainHome(),
     ),
   );
 }
