c2957d94e3c9a8ae52bacb234c0d466b4d3dc7d9
Dfg_Dfgh <dd180841@gmail.com>
2022-12-13 16:24:59 +0000

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:lafyuu/Cubits/cart_screen_cubit/cart_screen_cubit.dart';
 import 'package:lafyuu/Screens/main_.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:lafyuu/Thems/card_in_cart.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: MainHome(),
+    MultiProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CartCubit()..getCartDetails(),
+        ),
+      ],
+      child: MaterialApp(
+        home: MainHome(),
+      ),
     ),
   );
 }
