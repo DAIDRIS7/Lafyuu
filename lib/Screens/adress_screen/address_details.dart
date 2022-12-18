e075d5fee069f87cac3aa765912f83d71732b5c0
Dfg_Dfgh <dd180841@gmail.com>
2022-12-18 01:55:39 +0000

    .

---
+++ lib/Screens/adress_screen/address_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdressDetails extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return _buildbody();
+  }
+}
+dy: _buildbody(),);
+  }
+}
+
+_buildbody(){
+
+return Card();
+}
