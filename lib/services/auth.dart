d94d6e9041764c9dc70234855fc905121a2b5bfb
Dfg_Dfgh <dd180841@gmail.com>
2023-02-10 12:37:42 +0000

    Add Auth class

---
+++ lib/services/auth.dart
@@ -0,0 +1,24 @@
+class Auth {
+  Auth._privateConstructor();
+
+  static final Auth _instance = Auth._privateConstructor();
+
+  static Auth get instance => _instance;
+  Future<UserCredential> _signInByGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
